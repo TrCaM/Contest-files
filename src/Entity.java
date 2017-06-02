75bbb8d1fe14465ec7a0b05ce362b546e482c751
TrCaM <TriCao@cmail.carleton.ca>
2017-06-01 22:44:47 -0400

    Update quick IO

---
+++ src/Entity.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Gon on 2017-05-29.
+ */
+public class Entity {
+
+    protected int x;
+    protected int y;
+    protected int z;
+
+    public Entity(){
+        x=0;
+        y=0;
+        z=0;
+    }
+
+    public Entity(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
